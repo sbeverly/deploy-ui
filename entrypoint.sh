6cf5ccf5096a913a2ef99d12c34e175bb25e64a9
Siyan Beverly <siyan.beverly@gmail.com>
2020-04-13 01:26:05 +0000

    init repo

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file $2 --project-id $3; fi
+
+gsutil -h "Cache-Control: no-cache, max-age=0, must-revalidate" rsync -rd build/ $4
