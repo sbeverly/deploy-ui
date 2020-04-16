93cb87d846c9b7d1fe120892b7e4a662b6da25e5
Siyan Beverly <siyan.beverly@gmail.com>
2020-04-15 23:56:42 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file $2 --projec
 npm ci --prod
 npm run-script build
 
+echo $1
 echo $4
 
 gsutil -h "Cache-Control: no-cache, max-age=0, must-revalidate" rsync -rd ./build/ $4
