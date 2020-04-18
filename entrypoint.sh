97f732c5198df666fba84b635f067d4a7ec752a4
Siyan Beverly <siyan.beverly@gmail.com>
2020-04-18 00:45:19 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file ./sa_key.json --project $3; fi
+if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file /sa_key.json --project $3; fi
 
 npm ci --prod
 npm run-script build
