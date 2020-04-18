9550b5a9a95c0ffc7393ba0af048e2aeadf6a77f
Siyan Beverly <siyan.beverly@gmail.com>
2020-04-18 15:22:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo $1
 echo $2
 echo "END VARS"
 
-if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file /sa_key.json --project $3; fi
+if [[ $1 ]]; then gcloud auth activate-service-account $1 --key-file ./sa_key.json --project $3; fi
 
 npm ci --prod
 npm run-script build
