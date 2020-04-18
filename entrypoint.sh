e6dd2b9f36965ee7c74938a4c6170d509382b28c
Siyan Beverly <siyan.beverly@gmail.com>
2020-04-17 22:27:40 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [[ $1 ]]; then gcloud auth activate-service-account $INPUT_SA_EMAIL --key-file ./sa_key.json --project-id $INPUT_PROJECT_ID; fi
+if [[ $INPUT_SA_EMAIL ]]; then gcloud auth activate-service-account $INPUT_SA_EMAIL --key-file ./sa_key.json --project-id $INPUT_PROJECT_ID; fi
 
 npm ci --prod
 npm run-script build
 
-echo $1
-echo $4
+echo $INPUT_SA_EMAIL
+echo $INPUT_PROJECT_ID
 
 gsutil -h "Cache-Control: no-cache, max-age=0, must-revalidate" rsync -rd ./build/ $INPUT_GS_URL
